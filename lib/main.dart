7adcb762c4036de946a25d939d655013682d35a2
xcheche <chekwubechukwu23@gmail.com>
2023-11-09 03:24:18 +0100

    user can now fetch id via submit button

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,9 @@ class Album {
   }
 }
 
-Future<Album> fetchAlbum() async {
+Future<Album> fetchAlbum(int albumId) async {
   final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/2'));
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'));
 
   if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
@@ -41,11 +41,19 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late Future<Album> futureAlbum;
+  final TextEditingController _controller = TextEditingController();
+
+  void _fetchAlbulm() {
+    int albumId = int.tryParse(_controller.text) ?? 1;
+    setState(() {
+      futureAlbum = fetchAlbum(albumId);
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureAlbum = fetchAlbum(1);
   }
 
   @override
@@ -58,18 +66,31 @@ class _MyAppState extends State<MyApp> {
           title: const Text('GET HTTP EXAMPLE'),
           centerTitle: true,
         ),
-        body: FutureBuilder(
-          future: futureAlbum,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasData) {
-              return Text(snapshot.data!.title);
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            return const Text('No data available');
-          },
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Album ID Number'),
+              ),
+              ElevatedButton(
+                onPressed: _fetchAlbulm,
+                child: const Text('Submit'),
+              ),
+              FutureBuilder(
+                  future: futureAlbum,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!.title);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    return const CircularProgressIndicator();
+                  })
+            ],
+          ),
         ),
       ),
     );
